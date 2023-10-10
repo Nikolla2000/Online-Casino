2fa4fc1e13e19cfdee598fd50c0642459772f7df
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-10 16:33:27 +0300

    Made the images to be links to the games

--- client/src/Components/GameThumbnail/GameThumbnail.jsx
+++ client/src/Components/GameThumbnail/GameThumbnail.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import "./GameThumbnail.scss"
 
 const GameThumbnail = ({ data }) => {
   console.log(data.image);
   return (
     <div className='game-thumbnail-wrapper'>
-      <img src={data.image} alt='game-image'/>
+      <Link to={data.linkPath}>
+        <img src={data.image} alt='game-image'/>
+      </Link>
     </div>
   );
 };
