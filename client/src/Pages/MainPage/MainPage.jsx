093c033f0f5487ce37fc5848824f272275c89809
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-11 19:14:45 +0300

    Added a video background in the main page

--- client/src/Pages/MainPage/MainPage.jsx
+++ client/src/Pages/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { gamesData } from '../../Components/GameThumbnail/gamesData';
 import GameThumbnail from '../../Components/GameThumbnail/GameThumbnail';
 import "./MainPageStyles.scss"
+import videoBackground from '../../assets/images/background-video.mp4'
 
 const MainPage = () => {
   const [games, setGames] = useState(null)
@@ -11,17 +12,23 @@ const MainPage = () => {
   }, [])
   return (
     <div className='main-page-wrapper'>
-      <header>
-        <h1>Your Win Awaits</h1>
-      </header>
-      <section className='games-section'>
+      <video autoPlay loop muted className="video-background">
+        <source src={videoBackground} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <header>
+          <h1>Your Win Awaits</h1>
+        </header>
+      </div>
+      {/* <section className='games-section'>
         <h3>POPULAR GAMES</h3>
         <div className="games-wrapper">
           {gamesData.map((game, index) => (
             <GameThumbnail data={game} key={index + 1}/>
           ))}
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
