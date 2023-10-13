0a9d8950a5660e4e50175e023033385341bb8f35
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-13 16:10:24 +0300

    Took out the Header Section into its own component

--- client/src/Pages/MainPage/MainPage.jsx
+++ client/src/Pages/MainPage/MainPage.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import "./MainPageStyles.scss"
-import videoBackground from '../../assets/images/background-video2.mp4'
-import { Link } from 'react-router-dom';
-import "animate.css"
+
+import HeaderSection from './Sections/HeaderSection/HeaderSection';
 
 const MainPage = () => {
   return (
     <div className='main-page-wrapper'>
-      <div className="header-section">
-        <video autoPlay loop muted className="video-background">
-          <source src={videoBackground} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>'"animate__animated animate__bounce'
-        <header className="animate__animated animate__fadeIn">
-          <h1>Your Winnings Await</h1>
-          <div className="start-playing-btn">
-            <Link to='/games' class="glowButton" href="#">Start Playing</Link>
-          </div>
-        </header>
-      </div>
+      <HeaderSection/>
     </div>
   );
 };
---
+++ client/src/Pages/MainPage/Sections/HeaderSection/HeaderSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import videoBackground from '../../../../assets/images/background-video2.mp4'
+import { Link } from 'react-router-dom';
+import "animate.css"
+
+
+const HeaderSection = () => {
+  return (
+      <div className="header-section">
+        <video autoPlay loop muted className="video-background">
+          <source src={videoBackground} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <header className="animate__animated animate__fadeIn">
+          <h1>Your Winnings Await</h1>
+          <div className="start-playing-btn">
+            <Link to='/games' class="glowButton" href="#">Start Playing</Link>
+          </div>
+        </header>
+      </div>
+  );
+};
+
+export default HeaderSection;
