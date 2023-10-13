acb5e41f0780f549335028c360491613c75bf7b5
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-13 16:33:02 +0300

    Added and styled the introduction section

--- client/src/Pages/MainPage/MainPage.jsx
+++ client/src/Pages/MainPage/MainPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import "./MainPageStyles.scss"
 
 import HeaderSection from './Sections/HeaderSection/HeaderSection';
+import InstroductionSection from './Sections/IntroductionSection/InstroductionSection';
 
 const MainPage = () => {
   return (
     <div className='main-page-wrapper'>
       <HeaderSection/>
+      <InstroductionSection/>
     </div>
   );
 };
