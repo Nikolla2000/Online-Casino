4dc326315e725c86573b57d4143e0878093b5110
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-16 10:17:17 +0300

    Made the buttons with absolute position

--- client/src/Components/GameThumbnail/GameThumbnail.jsx
+++ client/src/Components/GameThumbnail/GameThumbnail.jsx
@@ -7,14 +7,14 @@ const GameThumbnail = ({ data }) => {
   console.log(data.image);
   return (
     <div className='game-wrapper'>
-    <div className='game-thumbnail-wrapper'>
-        <img src={data.image} alt='game-image'/>
-    </div>
+      <div className='game-thumbnail-wrapper'>
+          <img src={data.image} alt='game-image'/>
       <Link to={data.linkPath}>
         <Button variant='success' className='play-btn'>
           Play
         </Button>
       </Link>
+      </div>
     </div>
   );
 };
