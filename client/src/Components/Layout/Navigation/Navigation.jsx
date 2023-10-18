2ac6a89f6452256dd2e911e2bc34c2fc6d382a1c
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-18 18:51:10 +0300

    Wrote basic nav styles

--- client/src/Components/Layout/Navigation/Navigation.jsx
+++ client/src/Components/Layout/Navigation/Navigation.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import {  NavLink } from 'react-router-dom'
 import routes from '../../../utils/routes';
+import './NavigationStyles.scss'
 
 const Navigation = () => {
   return (
     <nav>
       <div className="nav-wrapper">
-        {Object.values(routes)
-        .filter((element) => element.includeInNav === true)
-        .map((element, index) => (
-          <NavLink
-            key={index + 1}
-            to={element.path}>
-            {element.name}  
-          </NavLink>
-        ))}
+        <div className="nav-menu">
+          {Object.values(routes)
+          .filter((element) => element.includeInNav === true)
+          .map((element, index) => (
+            <NavLink
+              key={index + 1}
+              to={element.path}>
+              {element.name}  
+            </NavLink>
+          ))}
+        </div>
       </div>
       
     </nav>
