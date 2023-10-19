8b2c41764b693bb66317305b47822f040466ef9c
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-19 20:16:39 +0300

    Set up register modal

--- client/src/Components/Layout/UserDropdown/UserDropdown.jsx
+++ client/src/Components/Layout/UserDropdown/UserDropdown.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UserDropdownStyles.scss'
+import RegisterForm from '../../Authentication/Register/RegisterForm';
 
 const UserDropdown = () => {
+  const [showRegisterModal, setShowRegisterModal] = useState(false)
+
+  const handleClose = () => {
+    setShowRegisterModal(false)
+  }
   return (
     <div className='user-dropdown'>
       <button>Login</button>
-      <button>Register</button>
+      <button onClick={() => setShowRegisterModal(true)}>Register</button>
+      {showRegisterModal && <RegisterForm handleClose={handleClose}/>}
     </div>
   );
 };
