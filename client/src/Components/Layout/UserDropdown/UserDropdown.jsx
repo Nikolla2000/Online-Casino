296a47b20993c99ac0f7879ca373252a9735dcae
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-19 19:50:21 +0300

    Installed material ui package

---
+++ client/src/Components/Layout/UserDropdown/UserDropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserDropdownStyles.scss'
+
+const UserDropdown = () => {
+  return (
+    <div className='user-dropdown'>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  );
+};
+
+export default UserDropdown;
