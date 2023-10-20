fc2d354c7c8c373b7827a147b372310a5eebfb55
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-20 17:03:37 +0300

    Created the login form

---
+++ client/src/Components/Authentication/Login/LoginForm.jsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import '../Register/RegisterStyles.scss'
+
+const LoginForm = ({ handleClose }) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted data:', formData);
+  };
+
+  return (
+    <div className='login-modal-wrapper'>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+      <Box sx={{ width: 400, color: '#fff' }}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Login
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+         <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+        </Typography>
+      </Box>
+    </Modal>
+    </div>
+  );
+};
+
+export default LoginForm;
