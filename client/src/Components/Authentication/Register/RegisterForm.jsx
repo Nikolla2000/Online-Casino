9a6def1c7b4d46ce853d157b461e506d5cf6e324
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-20 16:41:25 +0300

    Created the rregister form

--- client/src/Components/Authentication/Register/RegisterForm.jsx
+++ client/src/Components/Authentication/Register/RegisterForm.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import Modal from '@mui/material/Modal'; // Import the Modal component
-import Box from '@mui/material/Box'; // Import the Box component
-import Typography from '@mui/material/Typography'; // Import the Typography component
+import React, {useState} from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const RegisterForm = ({handleClose}) => {
+const RegisterForm = ({ handleClose}) => {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+    username: '',
+    email: '',
+    phone: '',
+    country: 'usa',
+    password: '',
+    confirm_password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted');
+  }
   return (
-    <div>
+    <div className='register-modal-wrapper'>
       <Modal
         open={open}
         onClose={handleClose}
@@ -17,7 +39,84 @@ const RegisterForm = ({handleClose}) => {
             Register
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <label>First Name</label>
+          <form onSubmit={handleSubmit}>
+        <label htmlFor="firstname">First Name:</label>
+        <input
+          type="text"
+          id="firstname"
+          name="firstname"
+          value={formData.firstname}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="lastname">Last Name:</label>
+        <input
+          type="text"
+          id="lastname"
+          name="lastname"
+          value={formData.lastname}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="phone">Phone Number:</label>
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="country">Country:</label>
+        <select
+          id="country"
+          name="country"
+          value={formData.country}
+          onChange={handleChange}
+        >
+          <option value="usa">USA</option>
+          <option value="canada">Canada</option>
+          <option value="uk">UK</option>
+          <option value="other">Other</option>
+        </select>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="confirm_password">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirm_password"
+          name="confirm_password"
+          value={formData.confirm_password}
+          onChange={handleChange}
+          required
+        />
+        <input type="submit" value="Register" />
+      </form>
           </Typography>
         </Box>
       </Modal>
