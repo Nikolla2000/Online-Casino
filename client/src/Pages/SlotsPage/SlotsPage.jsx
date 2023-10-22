60bb82ed56f49984fef9640fd77bfbe5b0ed3d56
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-22 21:47:12 +0300

    Slots structure

--- client/src/Pages/SlotsPage/SlotsPage.jsx
+++ client/src/Pages/SlotsPage/SlotsPage.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const SlotsPage = () => {
   return (
-    <div>
-      Slots Page
+    <div className='slots-page-wrapper'>
+      <div className="slots-game-wrapper">
+        <div className="slots-element-wrapper">
+            <div className="slot">1</div>
+            <div className="slot">2</div>
+            <div className="slot">3</div>
+        </div>
+      </div>
     </div>
   );
 };
