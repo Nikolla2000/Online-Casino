65f56489ac93b39f88a351ac0388ddfeb1579207
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-23 12:33:06 +0300

    Created the admin page

--- client/src/Pages/AdminPage/AdminPage.jsx
+++ client/src/Pages/AdminPage/AdminPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AdminPage = () => {
+  const [users ,setUsers] = useState(0)
   return (
     <div className='admin-page-wrapper'>
-      
+      <h1>Admin page</h1>
+      <article>
+        <h3>Users List:</h3>
+        {users?.length 
+        ? ( <ul>
+          {users.map((user, i) => (
+            <li key={i + 1}>Name: {user.username}</li>
+          ))}
+        </ul>
+        ) : <p>No users to display</p>}
+      </article>
     </div>
   );
 };
