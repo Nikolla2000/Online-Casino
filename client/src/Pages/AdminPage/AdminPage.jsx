d7711657a22d2c96d06c651c970d0063b63175a6
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-23 12:34:35 +0300

    Created the route for the admin page

--- client/src/Pages/AdminPage/AdminPage.jsx
+++ client/src/Pages/AdminPage/AdminPage.jsx
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const AdminPage = () => {
   const [users ,setUsers] = useState(0)
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axios.get('/users', {
+          signal: controller.signal
+        });
+        console.log(response.data);
+        isMounted && setUsers(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    }
+  }, [])
+  
   return (
     <div className='admin-page-wrapper'>
       <h1>Admin page</h1>
