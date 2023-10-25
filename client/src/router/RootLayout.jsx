30087bcfb7d7c889689b7844743abf786b21172f
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-25 11:11:45 +0300

    Added toaster to the register

--- client/src/router/RootLayout.jsx
+++ client/src/router/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useNavigation, Outlet } from 'react-router';
 import Navigation from '../Components/Layout/Navigation/Navigation';
+import { Toaster } from 'react-hot-toast'
 
 const RootLayout = () => {
   const navigation = useNavigation();
   return (
     <>
       <Navigation />
+      <Toaster position='bottom-right' toastOptions={{  duration: 2000 }}/>
       <main>
         {navigation.state === "loading" && (
           <h2 className="loading-msg">Loading...</h2>
