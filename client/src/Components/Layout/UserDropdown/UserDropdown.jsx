4d08129acfbca6ca238ca44c185a096d7b2f61d4
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-29 17:56:25 +0200

    Created an user dashboard page

--- client/src/Components/Layout/UserDropdown/UserDropdown.jsx
+++ client/src/Components/Layout/UserDropdown/UserDropdown.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './UserDropdownStyles.scss'
 import RegisterForm from '../../Authentication/Register/RegisterForm';
 import LoginForm from '../../Authentication/Login/LoginForm';
+import { UserContext } from '../../../../context/userContext';
 
 const UserDropdown = ({ show, play }) => {
   const [showRegisterModal, setShowRegisterModal] = useState(false)
   const [showLoginrModal, setShowLoginModal] = useState(false)
+  const {user} = useContext(UserContext)
 
   const handleClose = () => {
     setShowRegisterModal(false)
@@ -17,8 +19,11 @@ const UserDropdown = ({ show, play }) => {
       <div className="user-dropdown-img">
         <img src='../../../src/assets/images/user.png' alt='user-img' />
       </div>
+      {user && <h3 className='text-white text-xl'>Welcome, {user.name}!</h3>}
       <div className="dropdown-buttons">
-        <button onClick={() => setShowLoginModal(true)}>Login</button>
+        <button onClick={() => setShowLoginModal(true)}>
+          {!user ? 'Login' : 'Logout'}
+        </button>
         <button onClick={() => setShowRegisterModal(true)}>Register</button>
       </div>
       {showRegisterModal && <RegisterForm handleClose={handleClose}/>}
