fd958021fa6dd9b72b3387d8581584fd5bc6f813
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-05 19:24:10 +0200

    Wrote the logic for rendering of the images on the gameboard

--- client/src/Pages/SlotsPage/SlotsPage.jsx
+++ client/src/Pages/SlotsPage/SlotsPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './SlotsStyles.scss'
+import GameBoard from './GameBoard/GameBoard';
 const SlotsPage = () => {
   return (
     <div className='slots-page-wrapper'>
-      <div className="slots-game-wrapper">
+      {/* <div className="slots-game-wrapper">
         <div className="slots-element-wrapper">
             <div className="slot">1</div>
             <div className="slot">2</div>
             <div className="slot">3</div>
         </div>
-      </div>
+      </div> */}
+      <GameBoard/>
     </div>
   );
 };
