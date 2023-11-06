4f1d393a1eba74ce3dd5b8a4fd9650e1a893e60f
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-06 16:38:56 +0200

    Update bets

--- client/src/Pages/SlotsPage/Gadgets/GadgetsBar.jsx
+++ client/src/Pages/SlotsPage/Gadgets/GadgetsBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SpinButton from './SpinButton';
 import './GadgetsStyles.scss'
+import Bets from './Bets';
 
 const GadgetsBar = () => {
   return (
-    <div className='gadgets-bar'>
+    <div className='gadgets-bar flex flex-row'>
+      <Bets/>
       <SpinButton/>
     </div>
   );
