0edfbebf9875fd940c151008a25c38887d5e93e7
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-06 21:06:19 +0200

    styles

--- client/src/Pages/SlotsPage/Gadgets/Bets.jsx
+++ client/src/Pages/SlotsPage/Gadgets/Bets.jsx
@@ -4,12 +4,12 @@ import './GadgetsStyles.scss'
 const Bets = () => {
   return (
     <div className='bets-wrapper flex justify-center align-middle'>
-      <div className='minus'>-</div>
-      <div className='bet flex flex-col'>
-        <span className=''>Bet</span>
+      <div className='minus text-white cursor-pointer'>-</div>
+      <div className='bet flex flex-col bg-black'>
+        <span className='text-white text-uppercase text-center'>Bet</span>
         <span className='bet-value'>10000</span>
       </div>
-      <div className='plus'>+</div>
+      <div className='plus text-white cursor-pointer'>+</div>
     </div>
   );
 };
