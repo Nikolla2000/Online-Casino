0b8691109dd4d987a288302fdcf3903cb7656093
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-07 11:36:39 +0200

    Added the 'red or black' button
    It will be used when choosing the option to double the winning

--- client/src/Pages/SlotsPage/Gadgets/GadgetsBar.jsx
+++ client/src/Pages/SlotsPage/Gadgets/GadgetsBar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import SpinButton from './SpinButton';
 import './GadgetsStyles.scss'
 import Bets from './Bets';
+import ChooseCardColor from './ChooseCardColor';
 
 const GadgetsBar = () => {
   return (
     <div className='gadgets-bar flex flex-row'>
       <Bets/>
+      <ChooseCardColor/>
       <div className='max-bets-btn text-white'>
         <button className='uppercase'>max bet</button>
       </div>
