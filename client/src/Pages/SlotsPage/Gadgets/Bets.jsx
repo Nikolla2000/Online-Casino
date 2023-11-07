a30f3b9134eb37a1f9d41c56eb407cc53aa50726
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-07 09:49:41 +0200

    Styled the bets button

--- client/src/Pages/SlotsPage/Gadgets/Bets.jsx
+++ client/src/Pages/SlotsPage/Gadgets/Bets.jsx
@@ -7,7 +7,7 @@ const Bets = () => {
       <div className='minus text-white cursor-pointer'>-</div>
       <div className='bet flex flex-col bg-black'>
         <span className='text-white text-uppercase text-center'>Bet</span>
-        <span className='bet-value'>10000</span>
+        <span className='bet-value'>1000</span>
       </div>
       <div className='plus text-white cursor-pointer'>+</div>
     </div>
