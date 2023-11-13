95219910a7e4a2977d5c3e7809796b83f5fa52fd
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-13 20:15:39 +0200

    The autoplay is now functional

--- client/src/Pages/SlotsPage/Gadgets/SpinButton.jsx
+++ client/src/Pages/SlotsPage/Gadgets/SpinButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './GadgetsStyles.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import { startSpinning, stopSpinning, toggleAutoPlay } from '../../../redux/features/slots/slotMachineSlice';
@@ -11,8 +11,7 @@ const SpinButton = () => {
   const isSpinning = useSelector(state => state.slotMachine.isSpinning)
   const autoPlay = useSelector(state => state.slotMachine.autoPlay)
   const slots = useSelector(state => state.slotMachine.slots)
-  // console.log(isSpinning, slots);
-  console.log(autoPlay);
+  console.log(isSpinning, slots);
 
   const handleSpin = async () => {
     if(isSpinning) {
@@ -23,17 +22,40 @@ const SpinButton = () => {
 
     try {
       const response = await axios.get('/slots/spin');
-      // dispatch(stopSpinning(response.data.result));
     } catch (error) {
 
       console.error('Error spinning the slots:', error);
-      // dispatch(stopSpinning([]));
+      dispatch(stopSpinning([]));
     }
   };
 
   const toggle = () => {
     dispatch(toggleAutoPlay())
   }
+
+  const autoSpinIntervalRef = useRef(null)
+
+  useEffect(() => {
+    const startAutoSpin = () => {
+      autoSpinIntervalRef.current = setInterval(() => {
+        handleSpin();
+      }, 3000);
+    };
+
+    const stopAutoSpin = () => {
+      clearInterval(autoSpinIntervalRef.current);
+    };
+
+    if (autoPlay) {
+      startAutoSpin();
+    } else {
+      stopAutoSpin();
+    }
+
+    return () => {
+      stopAutoSpin();
+    };
+  }, [autoPlay, handleSpin]);
 
   return (
     <div className='spin-btn'>
