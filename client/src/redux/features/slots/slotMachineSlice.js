10e3174306c400c29deaf619c18d1f3ca3698236
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-19 18:00:45 +0200

    Wrote the logic for checking for a win

--- client/src/redux/features/slots/slotMachineSlice.js
+++ client/src/redux/features/slots/slotMachineSlice.js
@@ -26,6 +26,7 @@ export const slotMachineSlice = createSlice({
       //   state.isWin = false;
       //   state.winType = null;
       // }
+      isWinningCombination(state.slots)
     },
     toggleAutoPlay(state) {
       state.autoPlay = !state.autoPlay;
@@ -36,11 +37,30 @@ export const slotMachineSlice = createSlice({
   }
 })
 
-// const isWinningCombination = (slots) => {
-//   if (slots[0] === slots[1] && slots[1] === slots[2]) {
-    
-//   }
-// }
+const isWinningCombination = (slots) => {
+  for(let i = 0; i < slots.length; i++) {
+
+    if( slots[i][0] === slots[i][1] 
+     && slots[i][1] === slots[i][2]
+     && slots[i][2] === slots[i][3]
+     && slots[i][3] === slots[i][4]) {
+      alert('5 cols')
+
+    } else if (slots[i][0] === slots[i][1] 
+            && slots[i][1] === slots[i][2]
+            && slots[i][2] === slots[i][3]
+      ) {
+        alert('4 cols')
+      }
+      else if (slots[i][0] === slots[i][1] 
+            && slots[i][1] === slots[i][2]) {
+          alert('3 cols')
+        }
+      else if (slots[i][0] === slots[i][1]){
+        alert('2 cols')
+      }
+  }
+}
 
 export const { startSpinning, stopSpinning, toggleAutoPlay } = slotMachineSlice.actions;
 export default slotMachineSlice.reducer
