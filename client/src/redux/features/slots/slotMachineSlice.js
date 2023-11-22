b9a57f6c6ed9dccd828106e0e840ba9c3e588f3e
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-22 10:27:19 +0200

    Moved the betsslice state into the slotsslice

--- client/src/redux/features/slots/slotMachineSlice.js
+++ client/src/redux/features/slots/slotMachineSlice.js
@@ -8,7 +8,9 @@ const initialState = {
   isSpinning: false,
   isWin: false,
   winType: null,
-  autoPlay: false
+  autoPlay: false,
+  bet: 100,
+  totalCredits: null
 }
 
 export const updateCreditsOnServer = createAsyncThunk(
@@ -43,35 +45,93 @@ export const slotMachineSlice = createSlice({
     },
     autoPlayOff(state) {
       state.autoPlay = false;
-    }
+    },
+    increaseBet(state) {
+      if(state.bet === 1000) {
+        return
+      } else {
+        state.bet += 100
+      }
+    },
+    decreaseBet(state) {
+      if(state.bet === 100) {
+        return
+      } else {
+        state.bet -= 100
+      }
+    },
+    maxBet(state) {
+      state.bet = 1000;
+    },
+    maxCredits(state) {
+      state.totalCredits = 10000;
+    },
+    spendCredits(state) {
+      if(state.totalCredits < state.bet){
+        return
+      }
+      state.totalCredits -= state.bet
+    },
+    updateCredits(state, action) {
+      state.totalCredits = action.payload
+    },
+    twoColsWin(state) {
+      alert('bachka')
+      state.totalCredits += state.bet * 2
+    },
+    threeColsWin(state) {
+      state.totalCredits += state.bet * 10
+    },
+    fourColsWin(state) {
+      state.totalCredits += state.bet * 100
+    },
+    fiveColsWin(state) {
+      state.totalCredits += state.bet * 200
+    },
   },
 });
 
-// const isWinningCombination = (slots) => {
-//   for(let i = 0; i < slots.length; i++) {
+const checkSlotWin = (slots, callback) => {
 
-//     if( slots[i][0] === slots[i][1] 
-//      && slots[i][1] === slots[i][2]
-//      && slots[i][2] === slots[i][3]
-//      && slots[i][3] === slots[i][4]) {
-//       alert('5 cols')
+  for (let i = 0; i < slots.length; i++) {
+    if (
+      slots[i][0] === slots[i][1] &&
+      slots[i][1] === slots[i][2] &&
+      slots[i][2] === slots[i][3] &&
+      slots[i][3] === slots[i][4]
+    ) {
+      alert("5 cols");
+    } 
+    else if (
+      slots[i][0] === slots[i][1] &&
+      slots[i][1] === slots[i][2] &&
+      slots[i][2] === slots[i][3]
+    ) {
+      alert("4 cols");
+    } 
+    else if (slots[i][0] === slots[i][1] && slots[i][1] === slots[i][2]) {
+      alert("3 cols");
+    } 
+    else if (slots[i][0] === slots[i][1]) {
+      alert("2 cols");
+      twoColsWin()
+    }
+  }
+};
 
-//     } else if (slots[i][0] === slots[i][1] 
-//             && slots[i][1] === slots[i][2]
-//             && slots[i][2] === slots[i][3]
-//       ) {
-//         alert('4 cols')
-//       }
-//       else if (slots[i][0] === slots[i][1] 
-//             && slots[i][1] === slots[i][2]) {
-//           alert('3 cols')
-//         }
-//       else if (slots[i][0] === slots[i][1]){
-//         alert('2 cols')
-//         store.dispatch(twoColsWin())
-//       }
-//   }
-// }
+export const {startSpinning, 
+              stopSpinning, 
+              toggleAutoPlay,
+              increaseBet, 
+              decreaseBet, 
+              maxBet, 
+              maxCredits, 
+              spendCredits, 
+              updateCredits,
+              twoColsWin,
+              threeColsWin,
+              fourColsWin,
+              fiveColsWin
+            } = slotMachineSlice.actions;
 
-export const { startSpinning, stopSpinning, toggleAutoPlay } = slotMachineSlice.actions;
 export default slotMachineSlice.reducer
