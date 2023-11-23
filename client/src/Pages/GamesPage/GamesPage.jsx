7ee0c3f93f8cb04ca41a6bde83eb4f7fe5a387a7
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-23 19:08:33 +0200

    Game Page styles

--- client/src/Pages/GamesPage/GamesPage.jsx
+++ client/src/Pages/GamesPage/GamesPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { gamesData } from '../../Components/GameThumbnail/gamesData';
 import GameThumbnail from '../../Components/GameThumbnail/GameThumbnail';
+import backgroundVideo from "../../assets/images/games-page-background.mp4"
 import "./GamesPageStyles.scss"
 
 const GamesPage = () => {
   return (
     <div className='games-page-wrapper'>
+      <video src={backgroundVideo}></video>
        <section className='games-section'>
         <h2 className='games-page-heading'>TOP GAMES</h2>
         <div className="games-wrapper">
