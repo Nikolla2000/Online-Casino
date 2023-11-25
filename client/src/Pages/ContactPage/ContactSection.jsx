0ae324cfe9aaa08ba2d7dbbfd569defcb83994e0
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-25 21:35:51 +0200

    Created contact data

--- client/src/Pages/ContactPage/ContactSection.jsx
+++ client/src/Pages/ContactPage/ContactSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocketChat, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const ContactSection = ({ contactData }) => {
   return (
     <div>
       <div className="icon">
-        icon
+        <FontAwesomeIcon icon={contactData.icon} style={{color: "#fa16fe",}} />
       </div>
       <div className="section-heading"></div>
       <div className="section-descr"></div>
