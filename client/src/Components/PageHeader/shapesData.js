d70b83cd8e1e29539aa64be7f751c5c5cfe23720
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-25 21:08:13 +0200

    Added the shapes images

---
+++ client/src/Components/PageHeader/shapesData.js
@@ -0,0 +1,22 @@
+const shapesData = [
+  {
+      imgSrc: "/images/shapes/shape1.png"
+  },
+  {
+      imgSrc: "/images/shapes/shape2.png"
+  },
+  {
+      imgSrc: "/images/shapes/shape3.png"
+  },
+  {
+      imgSrc: "/images/shapes/shape4.png"
+  },
+  {
+      imgSrc: "/images/shapes/shape5.png"
+  },
+  {
+      imgSrc: "/images/shapes/shape6.png"
+  }
+]
+
+export default shapesData
