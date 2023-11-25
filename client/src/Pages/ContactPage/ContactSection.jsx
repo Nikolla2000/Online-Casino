3d4f11a2b391cc1ef684aa01fcacef6c95adb66b
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-25 21:19:54 +0200

    Contact Section Component template

---
+++ client/src/Pages/ContactPage/ContactSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactSection = ({ contactData }) => {
+  return (
+    <div>
+      <div className="icon">
+        icon
+      </div>
+      <div className="section-heading"></div>
+      <div className="section-descr"></div>
+    </div>
+  );
+};
+
+export default ContactSection;
