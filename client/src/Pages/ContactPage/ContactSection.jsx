babf7b3d283385d51660a203400bbad8fbef36ae
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-26 21:49:09 +0200

    Position the items with rows and cols from react bootstrap

--- client/src/Pages/ContactPage/ContactSection.jsx
+++ client/src/Pages/ContactPage/ContactSection.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ContactItem from './ContactItem';
 import { contactData } from './contactData';
+import { Row, Col } from "react-bootstrap";
 
 const ContactSection = () => {
   return (
-    <div className='contact-section-wrapper'>
-      {contactData.map((data, i) => (
-        <ContactItem data={data} key={i+1}/>
-      ))}
+    <div className='contact-section-wrapper container'>
+      <Row>
+        {contactData.map((data, i) => (
+          <Col lg={4} md={12}>
+            <ContactItem data={data} key={i+1}/>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
