2b8f13895bf249432f4e9e2e3c33c93625d6c4ad
Nikola Uzunov <nikollla2000@abv.bg>
2023-11-29 13:26:34 +0200

    Changed the background of the contact page

--- client/src/Pages/ContactPage/ContactPage.jsx
+++ client/src/Pages/ContactPage/ContactPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PageHeaderComponent from '../../Components/PageHeader/PageHeader';
 import ContactSection from './ContactSection';
+import "./ContactStyles.scss";
 
 const ContactPage = () => {
   return (
-    <div>
+    <div className='contact-page-wrapper'>
       <PageHeaderComponent heading={{title: "Contact", breadcrumb: "Contact"}}/>
       <ContactSection/>
     </div>
