0036dd61088efc6ab84500946fe82f9de9889ac2
Nikola Uzunov <nikollla2000@abv.bg>
2023-12-05 09:12:35 +0200

    Created the skeleton for the footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer-wrapper"></div>
+        <div className="footer-wrapper">
+            <div className="technologies-used-section">
+                <h3>This Website was made with the invaluable  help of</h3>
+                <div className="technologies-icons"></div>
+            </div>
+            <div className="social-medias-section">
+                <h3>Play Responsibly!</h3>
+                <div className="social-media-icons">
+                    React, NodeJs, HTML/SASS, JavaScript, ExpressJS, 
+                    MongoDb, Tailwind, Redux
+                </div>
+            </div>
+        </div>
     )
 }
 
