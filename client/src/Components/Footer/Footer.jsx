3c62e7b641582072aa4dd4c05e82bfda3a35d7f6
Nikola Uzunov <nikollla2000@abv.bg>
2023-12-17 16:52:49 +0200

    Placed and styled the icons

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -5,16 +5,42 @@ const Footer = () => {
     return (
         <div className="footer-wrapper">
             <div className="technologies-used-section">
-                <h3>This Website was made with the invaluable  help of</h3>
+                <h3>This Website was made by me - <span className='text-red-500'>Nikola Uzunov</span>, with the help of:</h3>
                 <div className="technologies-icons">
-                    <img src="../../src/assets/images/technologies-icons/react-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/nodejs-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/html-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/css-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/javascript-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/mongodb-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/redux-icon.svg" alt="technology-icon" />
-                    <img src="../../src/assets/images/technologies-icons/sass-icon.svg" alt="technology-icon" />
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/html-icon.svg" alt="technology-icon" />
+                        <p><span>HTML</span></p>
+                    </div>
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/javascript-icon.svg" alt="technology-icon" />
+                        <p><span>Javascript</span></p>
+                    </div>
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/css-icon.svg" alt="technology-icon" />
+                        <p><span>CSS</span></p>
+                    </div>
+                </div>
+                <div className="technologies-icons row-2">
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/react-icon.svg" alt="technology-icon" />
+                        <p><span>ReactJS</span></p>
+                    </div>
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/nodejs-icon.svg" alt="technology-icon" />
+                        <p><span>NodeJS</span></p>
+                    </div>
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/sass-icon.svg" alt="technology-icon" />
+                        <p><span>SASS</span></p>
+                    </div>
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/mongodb-icon.svg" alt="technology-icon" />
+                        <p><span>MongoDB</span></p>
+                    </div>
+                    <div className="icon">
+                        <img src="../../src/assets/images/technologies-icons/redux-icon.svg" alt="technology-icon" />
+                        <p><span>Redux</span></p>
+                    </div>
                 </div>
             </div>
             <div className="social-medias-section">
