4ab799f234e0d1bf7b75942db93c26da8a71866f
Nikola Uzunov <nikollla2000@abv.bg>
2023-12-17 10:17:27 +0200

    Added the images to the footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import "./FooterStyles.scss";
 
 const Footer = () => {
     return (
         <div className="footer-wrapper">
             <div className="technologies-used-section">
                 <h3>This Website was made with the invaluable  help of</h3>
-                <div className="technologies-icons"></div>
+                <div className="technologies-icons">
+                    <img src="../../src/assets/images/technologies-icons/react-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/nodejs-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/html-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/css-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/javascript-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/mongodb-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/redux-icon.svg" alt="technology-icon" />
+                    <img src="../../src/assets/images/technologies-icons/sass-icon.svg" alt="technology-icon" />
+                </div>
             </div>
             <div className="social-medias-section">
                 <h3>Play Responsibly!</h3>
