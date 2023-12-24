63d235c57971d7d710c25a9ddbb5c16236adcac8
Nikola Uzunov <nikollla2000@abv.bg>
2023-12-24 22:22:07 +0200

    Working profile navigation

--- client/src/Pages/User/Dashboard/Dashboard.jsx
+++ client/src/Pages/User/Dashboard/Dashboard.jsx
@@ -2,11 +2,17 @@ import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../../../context/userContext';
 import CircularProgress from '@mui/material/CircularProgress';
 import "./DashboardStyles.scss";
+import Stats from './Stats';
+import AccountInfo from './AccountInfo';
+import axios from '../../../axiosConfig';
+import { useNavigate } from 'react-router';
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
-
+  const [midSection, setMidSection] = useState('Stats')
+console.log(midSection);
   useEffect(() => {
     const waitForUser = async () => {
       return !!user.name;
@@ -24,6 +30,21 @@ const Dashboard = () => {
     return <div><CircularProgress color="secondary" /></div>;
   }
 
+  const changeSection = (event) => {
+    event.stopPropagation();
+    setMidSection(event.target.innerText);
+  }
+
+  const logout = async () => {
+      try {
+        await axios.get('/user/logout');
+        navigate('/');
+        location.reload();
+      } catch (error) {
+        console.error('Logout error:', error);
+      }
+  };
+
   return (
     <div className='dashboard-wrapper container'>
 
@@ -37,13 +58,14 @@ const Dashboard = () => {
           <h5>Master of the slots</h5>
         </div>
         <div className="profile-nav">
-          <p className='current'>Stats</p>
-          <p>Account Info</p>
-          <p>Logout</p>
+          <p className={`${midSection == "Stats" ? 'current' : ''}`} onClick={changeSection}>Stats</p>
+          <p className={`${midSection == "Account Info" ? 'current' : ''}`} onClick={changeSection}>Account Info</p>
+          <p onClick={logout}>Logout</p>
         </div>
       </div>
 
       <div className="mid-section">
+        {midSection == 'Stats' ? <Stats/> : <AccountInfo/>}
       </div>
 
       <div className="right-section"></div>
--- client/src/Pages/User/Dashboard/Stats.jsx
+++ client/src/Pages/User/Dashboard/Stats.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Stats = () => {
   return (
-    <div className='stats-wrapper'>
-      
+    <div className='stats-component-wrapper'>
+      <h2>Stats</h2>
+      <div className="stats">'
+      <div className="stat">
+          <span>Stat</span>
+          <span>Stat</span>
+      </div>
+      <div className="stat">
+          <span>Stat</span>
+          <span>Stat</span>
+      </div>
+      <div className="stat">
+          <span>Stat</span>
+          <span>Stat</span>
+      </div>
+      </div>
     </div>
   );
 };
