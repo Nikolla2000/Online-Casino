c73d9ab19f99d554d7dd350118fdd2d64cd3884c
Nikola Uzunov <nikollla2000@abv.bg>
2023-12-25 21:55:25 +0200

    Fixed the dissaperearing heading bug on the main page

--- client/src/Pages/MainPage/Sections/HeaderSection/HeaderSection.jsx
+++ client/src/Pages/MainPage/Sections/HeaderSection/HeaderSection.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import "animate.css"
 
 const HeaderSection = () => {
-  const [headerClass, setHeaderClass] = useState('');
+  const [headerClass, setHeaderClass] = useState('hidden');
   const [buttonClass, setButtonClass] = useState('hidden');
 
   useEffect(() => {
