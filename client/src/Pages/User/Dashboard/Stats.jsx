b4825b31c8bb2a5d4624311fdf5e9c9b3a5e7aed
Nikola Uzunov <nikollla2000@abv.bg>
2023-12-26 10:23:11 +0200

    Created stats data

--- client/src/Pages/User/Dashboard/Stats.jsx
+++ client/src/Pages/User/Dashboard/Stats.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import { statsData } from './statsData';
 
 const Stats = () => {
   return (
     <div className='stats-component-wrapper'>
       <h2>Stats</h2>
-      <div className="stats">
-      <div className="stat">
-          <span>Stat</span>
-          <span>Stat</span>
-      </div>
-      <div className="stat">
-          <span>Stat</span>
-          <span>Stat</span>
-      </div>
-      <div className="stat">
-          <span>Stat</span>
-          <span>Stat</span>
-      </div>
-      </div>
+      <div className="stats" >
+        {statsData.map((stat, i) => (
+          <div className="stat" key={i + 1}>
+            <span>{stat.game}</span>
+            <span>{stat.hoursPlayed}</span>
+            <span>{stat.creditsWon}</span>
+            <span>{stat.creditsSpend}</span>
+            <span>{stat.winRate}</span>
+          </div>
+        ))}
+        </div>
     </div>
   );
 };
---
+++ client/src/Pages/User/Dashboard/statsData.js
@@ -0,0 +1,30 @@
+export const statsData = [
+  {
+    game: "",
+    hoursPlayed: "Hours Played",
+    creditsWon: "Credits Won",
+    creditsSpend: "Credits Spend",
+    winRate: "Win Rate",
+  },
+  {
+    game: "Total",
+    hoursPlayed: "3",
+    creditsWon: statsData[3].creditsWon + statsData[4].creditsWon,
+    creditsSpend: "",
+    winRate: "",
+  },
+  {
+    game: "Slots",
+    hoursPlayed: "2",
+    creditsWon: "147.52",
+    creditsSpend: "50.00",
+    winRate: "52.30",
+  },
+  {
+    game: "Roulette",
+    hoursPlayed: "1",
+    creditsWon: "65.20",
+    creditsSpend: "60.00",
+    winRate: "49.80",
+  },
+]
