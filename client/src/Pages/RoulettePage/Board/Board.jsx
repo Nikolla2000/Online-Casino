d9869ced17c31d2895e4db338aa8306b77010b65
Nikola Uzunov <nikollla2000@abv.bg>
2024-01-07 21:25:58 +0200

    Red and Black numbers

--- client/src/Pages/RoulettePage/Board/Board.jsx
+++ client/src/Pages/RoulettePage/Board/Board.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
 const Board = () => {
+  const blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
+
+  const isBlackNumber = (num) => {
+    return blackNumbers.includes(num) ? 'black-number' : '';
+  };
+  
+
   const generateTable = () => {
     const rows = [];
 
     for (let i = 1; i <= 36; i += 3) {
       rows.push(
         <div key={i} className='board-row'>
-          <div>{i + 2}</div>
-          <div>{i + 1}</div>
-          <div>{i}</div>
+          <div className={isBlackNumber(i)}>{i + 2}</div>
+          <div className={isBlackNumber(i)}>{i + 1}</div>
+          <div className={isBlackNumber(i)}>{i}</div>
         </div>
       );
     }
