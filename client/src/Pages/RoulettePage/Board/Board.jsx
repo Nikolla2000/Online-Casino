132b897df5026196c2f83a1ffac7b983bb952eb5
Nikola Uzunov <nikollla2000@abv.bg>
2024-01-07 21:06:17 +0200

    Board changes

--- client/src/Pages/RoulettePage/Board/Board.jsx
+++ client/src/Pages/RoulettePage/Board/Board.jsx
@@ -6,10 +6,25 @@ const Board = () => {
 
     for (let i = 1; i <= 36; i += 3) {
       rows.push(
+        <div key={i} className='rowche'>
+          <div>{i + 2}</div>
+          <div>{i + 1}</div>
+          <div>{i}</div>
+        </div>
+      );
+    }
+
+    return rows;
+  };
+  const generateTable2 = () => {
+    const rows = [];
+
+    for (let i = 1; i <= 36; i += 3) {
+      rows.push(
         <tr key={i}>
-          <td>{i}</td>
-          <td>{i + 1}</td>
           <td>{i + 2}</td>
+          <td>{i + 1}</td>
+          <td>{i}</td>
         </tr>
       );
     }
@@ -19,10 +34,10 @@ const Board = () => {
 
   return (
     <div className='roulette-board'>
-      <div className="roulette-table-wrapper">
-        <table>
-          <tbody>{generateTable()}</tbody>
-        </table>
+      <div className="table-board-wrapper">
+        <div className='board'>
+          {generateTable()}
+        </div>
       </div>
     </div>
   );
--- client/src/Pages/RoulettePage/RoulettePage.jsx
+++ client/src/Pages/RoulettePage/RoulettePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
 import Wheel from './Wheel/Wheel';
 import Board from './Board/Board';
 import "./RouletteStyles.scss";
@@ -6,8 +7,16 @@ import "./RouletteStyles.scss";
 const RoulettePage = () => {
   return (
     <div className='roulette-page-wrapper'>
-      <Wheel/>
-      <Board/>
+      <Container>
+        <Row>
+          <Col>
+            <Wheel/>
+          </Col>
+          <Col>
+            <Board/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
