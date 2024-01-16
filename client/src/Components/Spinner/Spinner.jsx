97a516e60f837d09679a3ebc5f02a5f3084fc4e6
Nikola Uzunov <nikollla2000@abv.bg>
2024-01-16 09:59:33 +0200

    Added loading spinner

---
+++ client/src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+const LoadingSpinner = () => {
+    return (
+        <Spinner animation="border" variant="primary" />
+    )
+}
+
+export default LoadingSpinner;
--- client/src/Pages/GamesPage/GamesPage.jsx
+++ client/src/Pages/GamesPage/GamesPage.jsx
@@ -3,31 +3,36 @@ import { gamesData } from '../../Components/GameThumbnail/gamesData';
 import GameThumbnail from '../../Components/GameThumbnail/GameThumbnail';
 import backgroundVideo from "../../assets/images/games-page-background.mp4"
 import "./GamesPageStyles.scss"
+import LoadingSpinner from '../../Components/Spinner/Spinner';
 
 const GamesPage = () => {
   const [gameOneAnimation, setGameOneAnimation] = useState('animate__animated animate__fadeInRight');
   const [gameTwoAnimation, setGameTwoAnimation] = useState('');
-  
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timeoitId = setTimeout(() => {
       setGameTwoAnimation('animate__animated animate__fadeInRight')
-      
-    }, 300)
-
+    }, 400)
+    
+    setIsLoading(false);
     return () => clearTimeout(timeoitId);
   }, [])
 
   return (
     <div className='games-page-wrapper'>
       <video src={backgroundVideo}></video>
-       <section className='games-section'>
+      <section className='games-section'>
         <h2 className='games-page-heading'>TOP <span>GAMES</span></h2>
-        <div className="games-wrapper">
-          {gamesData.map((game, index) => (
-            <GameThumbnail data={game} key={index + 1} firstAnimation={gameOneAnimation} secondAnimation={gameTwoAnimation}/>
-          ))}
-        </div>
+        {isLoading ? (
+          <LoadingSpinner/> 
+        ) : (
+          <div className="games-wrapper">
+            {gamesData.map((game, index) => (
+              <GameThumbnail data={game} key={index + 1} firstAnimation={gameOneAnimation} secondAnimation={gameTwoAnimation} />
+            ))}
+          </div>
+        )}
       </section>
     </div>
   );
