0fdae396e3fc822243727889c9e70d6e9cda0c63
Nikola Uzunov <nikollla2000@abv.bg>
2024-07-07 17:34:53 +0300

    feat: added chosing a betting option

--- client/src/Pages/RoulettePage/Board/Board.jsx
+++ client/src/Pages/RoulettePage/Board/Board.jsx
@@ -17,7 +17,9 @@ const Board = () => {
   const [hasFirstStarted, setHasFirstStarted] = useState(false);
   // const [game, setGame] = useState(null);
   const chips = [5, 10, 25, 50, 100];
+
   const [bet, setBet] = useState(0);
+  const [chosenOption, setChosenOption] = useState(null);
 
   useEffect(() => {
     const fetchCredits = async () => {
@@ -30,13 +32,22 @@ const Board = () => {
 
     fetchCredits();
     const game = new Game(totalCredits);
+    game.getResult();
   }, [user, dispatch]);
 
-  const changeBet = (amount) => {
+
+  //Increase Bet function
+  const increaseBet = (amount) => {
     setBet((oldBet) => {
       const newBet = oldBet + amount;
       return newBet >= 500 ? 500 : newBet;
     })
+  }
+
+
+  //Function that sets the result that you want to bet on
+  const chooseOption = (option) => {
+    setChosenOption(oldOption => oldOption == option ? null : option);
   }
 
 
@@ -97,9 +108,9 @@ const Board = () => {
     for (let i = 1; i <= 36; i += 3) {
       rows.push(
         <div key={i} className='board-row'>
-          <div className={isBlackNumber(i + 2)}>{i + 2}</div>
-          <div className={isBlackNumber(i + 1)}>{i + 1}</div>
-          <div className={isBlackNumber(i)}>{i}</div>
+          <div className={isBlackNumber(i + 2)} value={i + 2} onClick={() => chooseOption(i + 2)}>{i + 2}</div>
+          <div className={isBlackNumber(i + 1)} value={i + 1} onClick={() => chooseOption(i + 1)}>{i + 1}</div>
+          <div className={isBlackNumber(i)} value={i} onClick={() => chooseOption(i)}>{i}</div>
         </div>
       );
     }
@@ -112,7 +123,7 @@ const Board = () => {
 
       <div className='board'>
         <div className="board-top-side">
-          <div className='zero'><span>0</span></div>
+          <div className='zero' onClick={() => chooseOption(0)}><span>0</span></div>
           {generateTable()}
           <div className='two-one'>
             <div>2:1</div>
@@ -124,19 +135,19 @@ const Board = () => {
         <div className="board-bottom-side">
           <div className='bet-options'>
             <div className='option'>
-              <div>1<sup>st</sup> 12</div>
-              <div className='left-border'>1 to 18</div>
-              <div>EVEN</div>
+              <div onClick={() => chooseOption("1st 12")}>1<sup>st</sup> 12</div>
+              <div className='left-border' onClick={() => chooseOption("1 to 18")}>1 to 18</div>
+              <div onClick={() => chooseOption("EVEN")}>EVEN</div>
             </div>
             <div className='option'>
-              <div>2<sup>nd</sup> 12</div>
-              <div className='red'>black</div>
-              <div className='black'>black</div>
+              <div onClick={() => chooseOption("2nd 12")}>2<sup>nd</sup> 12</div>
+              <div className='red' onClick={() => chooseOption("RED")}>black</div>
+              <div className='black' onClick={() => chooseOption("BLACK")}>black</div>
             </div>
             <div className='option'>
-              <div>3<sup>rd</sup> 12</div>
-              <div>ODD</div>
-              <div className='right-border'>19 to 36</div>
+              <div onClick={() => chooseOption("3rd 12")}>3<sup>rd</sup> 12</div>
+              <div onClick={() => chooseOption("ODD")}>ODD</div>
+              <div className='right-border' onClick={() => chooseOption("19 to 36")}>19 to 36</div>
             </div>
           </div>
         </div>
@@ -162,7 +173,7 @@ const Board = () => {
               value={chip} 
               alt={`${chip} chip`}
               key={chip}
-              onClick={() => changeBet(chip)}/> 
+              onClick={() => increaseBet(chip)}/> 
           ))}
           <button className='place-bet-btn'>Place Bet</button>
         </div>
@@ -170,7 +181,8 @@ const Board = () => {
       <div className='credits-info'>
         <p>Credits: {totalCredits}</p>
         <p>Bet: {bet}</p>
-        <p>Win: 0</p>
+        {/* <p>Win: 0</p> */}
+        <p>Chosen option: {chosenOption}</p>
       </div>
     </div>
   );
--- client/src/Pages/RoulettePage/game.js
+++ client/src/Pages/RoulettePage/game.js
@@ -1,14 +1,20 @@
 export class Game {
+  #numbers;
+  blackNumbers;
 
   constructor() {
-    console.log('test')
+    this.#numbers = Array.from({ length: 37 }, (_, i) => i);
+    this.blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
+    console.log(this.#numbers);
   }
 
   // function startGame() {
 
   // }
 
-  // function increaseBet() {
-
-  // }
+  getResult() {
+    const randomIndex = Math.floor(Math.random() * this.#numbers.length);
+    const randomNumber = this.#numbers[randomIndex];
+    return randomNumber;
+  }
 }
