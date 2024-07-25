126c8ecfaff002db980b10019c7ac97ea57329bc
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2024-07-25 13:32:08 +0300

    Update gamesData.js

--- client/src/Components/GameThumbnail/gamesData.js
+++ client/src/Components/GameThumbnail/gamesData.js
@@ -2,13 +2,13 @@ export const gamesData = [
   {
     gameName: "Slots",
     linkPath: '/games/slots',
-    image: '../../src/assets/images/slot-thumbnail-bg.mp4'
+    image: '/images/slot-thumbnail-bg.mp4'
   },
   {
     gameName: "Roulette",
     linkPath: '/games/roulette',
-    image: '../../src/assets/images/roulette/roulette-thumbnail-bg.mp4',
-    rouletteImg: "../../../src/assets/images/roulette/roulette-wheel-nobg.png"
+    image: '/images/roulette/roulette-thumbnail-bg.mp4',
+    rouletteImg: "/images/roulette/roulette-wheel-nobg.png"
 
   }
 ]
