157973dcd9a35fcf1cab984d455cbfaef5bdd770
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2024-07-25 14:05:33 +0300

    Frontend deploy url path fix

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 //cors
 app.use(cors({
   // origin: 'http://localhost:5173',
-  origin: 'https://uzu-online-casino.netlify.app/',
+  origin: 'https://uzu-online-casino.netlify.app',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true
