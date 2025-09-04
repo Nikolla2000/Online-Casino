4cea8677ef876a5e52ec2fc789075a288423039c
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-09-04 16:02:48 +0300

    feat: Chat database model

---
+++ server/models/Chat.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }],
+    lastMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message'
+    },
+    lastActivity: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true });
+
+chatSchema.index({ participants: 1 });
+chatSchema.index({ lastActivity: -1 });
+
+module.exports = mongoose.model('Chat', chatSchema);
