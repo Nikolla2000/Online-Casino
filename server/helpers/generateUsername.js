1802a136a72d0b0dd59ac765877441f475bc72cd
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-10-06 14:15:41 +0300

    feat: generate-username function
    Util function for creating usernames for users who sign up from google with oauth2. It taskes first, last name and email and returns the generated username.

---
+++ server/helpers/generateUsername.js
@@ -0,0 +1,16 @@
+function generateUsername(email, firstName, lastName) {
+    const first = (firstName || '').trim().toLowerCase();
+    const last = (lastName || '').trim().toLowerCase();
+    
+    if (first && last) {
+        return `${first}.${last}${Math.floor(Math.random() * 1000)}`;
+    } else if (first) {
+        return `${first}${Math.floor(Math.random() * 10000)}`;
+    } else if (last) {
+        return `${last}${Math.floor(Math.random() * 10000)}`;
+    } else {
+        return `${email.split('@')[0].toLowerCase()}${Math.floor(Math.random() * 1000)}`;
+    }
+}
+
+console.log(generateUsername('nikolla.uzunov@gmail.com', 'Nikola'));
