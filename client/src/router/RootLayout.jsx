63fb7bec3b95339a7ffd382d2cef721c1a187260
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-10-27 11:53:40 +0200

    hide chat popup when changing the page

--- client/src/router/RootLayout.jsx
+++ client/src/router/RootLayout.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { useNavigation, Outlet } from 'react-router';
+import React, { useEffect } from 'react';
+import { useNavigation, Outlet, useLocation } from 'react-router';
 import Navigation from '../Components/Layout/Navigation/Navigation';
 import { Toaster } from 'react-hot-toast'
 import LiveChatModal from '../Components/LiveChat/LiveChatModal';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import FloatingChatButton from '../Components/AIChatSupport/FloatingChatButton';
 import AIChatWidget from '../Components/AIChatSupport/AIChatWidget';
+import { hideChat } from '../redux/features/aiChatbot/aiChatbotSlice';
 
 const RootLayout = () => {
   const navigation = useNavigation();
   const { activeChat } = useSelector(state => state.chat);
   const { showAiChatWidget } = useSelector(state => state.aiChatbot);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(hideChat());
+  }, [location.pathname, dispatch]);
 
   return (
     <>
