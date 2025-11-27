7baae912ab94add852a011ee47db21c6a319352b
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-11-27 14:07:30 +0200

    refactor: clear up GameBoard component of unused imports and functions

--- client/src/Pages/SlotsPage/GameBoard/GameBoard.jsx
+++ client/src/Pages/SlotsPage/GameBoard/GameBoard.jsx
@@ -1,91 +1,10 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fiveColsWin, fourColsWin, startSpinning, stopSpinning, threeColsWin, twoColsWin } from '../../../redux/features/slots/slotMachineSlice';
-import { UserContext } from '../../../../context/userContext'
-import axios from '../../../axiosConfig';
+import { useSelector } from 'react-redux';
 
 const GameBoard = () => {
-  const dispatch = useDispatch();
-  const isSpinning = useSelector(state => state.slotMachine.isSpinning);
-  const totalCredits = useSelector(state => state.slotMachine.totalCredits)
   const slots = useSelector(state => state.slotMachine.slots);
-  const [spinningSlots, setSpinningSlots] = useState(slots);
-  const [isWin, setIsWin] = useState(false)
-  const { user } = useContext(UserContext)
-  
-  const checkSlotWin = async (slots) => {
-  const audio = new Audio('/sounds/slot-win-sound.mp3');
-  setIsWin(false)
 
-    for (let i = 0; i < slots.length; i++) {
-      if (
-        slots[i][0] === slots[i][1] &&
-        slots[i][1] === slots[i][2] &&
-        slots[i][2] === slots[i][3] &&
-        slots[i][3] === slots[i][4]
-        ) {
-          setIsWin(true)
-          dispatch(fiveColsWin())
-          audio.play();
-        } 
-        else if (
-          slots[i][0] === slots[i][1] &&
-          slots[i][1] === slots[i][2] &&
-          slots[i][2] === slots[i][3]
-          ) {
-        setIsWin(true)
-        dispatch(fourColsWin())
-        audio.play();
-        } 
-        else if (slots[i][0] === slots[i][1] && slots[i][1] === slots[i][2]) {
-          setIsWin(true)
-          dispatch(threeColsWin())
-          audio.play();
-        } 
-        else if (slots[i][0] === slots[i][1]) {
-          setIsWin(true)
-          dispatch(twoColsWin())
-          audio.play();
-        }
-      }
-    
-    if(isWin === true) {
-      setIsWin(false)
-      try {
-        await axios.put('/user/updateCredits', { userId: user.id, totalCredits})
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-
-  // useEffect(() => {
-  //   if (isSpinning) {
-  //     const spinInterval = setInterval(() => {
-  //       setSpinningSlots(generateRandomSlots());
-  //     }, 50);
-
-  //     setTimeout(() => {
-  //       clearInterval(spinInterval);
-  //       const finalSlots = generateFinalSlots()
-  //       dispatch(stopSpinning(finalSlots));
-  //       checkSlotWin(finalSlots)
-  //     }, 2000);
-
-  //     return () => {
-  //       clearInterval(spinInterval);
-  //     };
-  //   }
-  // }, [isSpinning, dispatch]);
-
-  // const generateRandomSlots = () => {
-  //   return slots.map(row => row.map(() => Math.floor(Math.random() * 12 + 1)));
-  // };
-
-  // const generateFinalSlots = () => {
-  //   return slots.map(row => row.map(() => Math.floor(Math.random() * 12 + 1)));
-  // };
   if (!slots) return;
+
   return (
     <div className='gameboard-wrapper'>
       {slots.map((row, i) => (
