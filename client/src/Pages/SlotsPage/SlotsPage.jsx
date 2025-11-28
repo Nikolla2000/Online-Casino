7ab6a736c261c40594c31bbcfb3fdf4cdbdb63ca
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-11-28 10:47:10 +0200

    slots - visual design upgrades

--- client/src/Pages/SlotsPage/SlotsPage.jsx
+++ client/src/Pages/SlotsPage/SlotsPage.jsx
@@ -2,20 +2,12 @@ import React from 'react';
 import './SlotsStyles.scss'
 import GameBoard from './GameBoard/GameBoard';
 import GadgetsBar from './Gadgets/GadgetsBar';
+
 const SlotsPage = () => {
-
-  const randomSlotItem = () => {
-    const randomSlotItem = Math.floor(Math.random() * 12 + 1);
-    if (randomSlotItem < 10) {
-      return `0${randomSlotItem}`
-    } else {
-      return randomSlotItem
-    }
-  }
-
+  
   return (
     <div className='slots-page-wrapper'>
-      <GameBoard randomSlotItem={randomSlotItem}/>
+      <GameBoard/>
       <GadgetsBar/>
     </div>
   );
