dd78ae6351e359d2284811213a1be55b7502c717
Nikola Uzunov <nikollla2000@abv.bg>
2025-12-16 22:00:07 +0200

    feat: different error types extended from AppError class

---
+++ server/helpers/errors.js
@@ -0,0 +1,34 @@
+// utils/errors.js
+
+const { AppError } = require("../middleware/errorHandler");
+
+class ValidationError extends AppError {
+  constructor(message) {
+    super(message, 400);
+  }
+}
+
+class NotFoundError extends AppError {
+  constructor(resource = 'Resource') {
+    super(`${resource} not found`, 404);
+  }
+}
+
+class UnauthorizedError extends AppError {
+  constructor(message = 'Unauthorized access') {
+    super(message, 401);
+  }
+}
+
+class ForbiddenError extends AppError {
+  constructor(message = 'Forbidden') {
+    super(message, 403);
+  }
+}
+
+module.exports = {
+  ValidationError,
+  NotFoundError,
+  UnauthorizedError,
+  ForbiddenError
+};
