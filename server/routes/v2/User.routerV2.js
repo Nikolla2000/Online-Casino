b19b8282d02a64a63a94b8ae301ea2695c10b142
Nikola Uzunov <nikollla2000@abv.bg>
2026-01-18 22:21:11 +0200

    new v2 register endpoint - router

--- server/routes/v2/User.routerV2.js
+++ server/routes/v2/User.routerV2.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { verifyJWT } = require('../../middleware/authentication');
-const { getUsers, getTotalCredits } = require('../../controllers/User.controller');
+const { getUsers, getTotalCredits, registerUserV2 } = require('../../controllers/User.controller');
+const { validateRequest } = require('../../middleware/validateRequest');
+const { default: z } = require('zod');
+const { registerSchema } = require('../../validation-schemas/user.schema');
 const router = express.Router();
 
 router.get('/', verifyJWT, getUsers);
 router.get('/:userId/credits', verifyJWT, getTotalCredits);
+router.post('/register', validateRequest(z.object({ body: registerSchema })), registerUserV2);
 
 module.exports = router;
