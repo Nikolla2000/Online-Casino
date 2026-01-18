0325b1c6d09584bb09a0c861bb146fafa75acdc0
Nikola Uzunov <nikollla2000@abv.bg>
2026-01-18 19:34:04 +0200

    create zod schema for validating user registration

---
+++ server/validation-schemas/user.schema.js
@@ -0,0 +1,79 @@
+const { z } = require('zod');
+
+const emailSchema = z.email("Invalid email format")
+  .toLowerCase()
+  .trim();
+
+const usernameSchema = z.string()
+  .min(4, 'Username must be at least 4 charactes')
+  .max(20, 'Username can\'nt be longer than 20 characters ')
+  .regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers and underscores')
+  .toLowerCase()
+  .trim();
+
+
+const passwordSchema = z.string()
+  .min(6, 'Password must be at least 6 characters')
+  .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+  .regex(/\d/, 'Password must contain at least one number')
+  .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character');
+
+
+const registerSchema = z.object({
+  firstName: z.string()
+    .min(2, 'First name must be at least 2 characters')
+    .max(20, 'First name can\'nt be longer than 20 characters')
+    .trim(),
+
+    lastName: z.string()
+    .min(2, 'Last name must be at least 2 characters')
+    .max(20, 'Last name can\'nt be longer than 20 characters')
+    .trim(),
+
+    username: usernameSchema,
+
+    email: emailSchema,
+
+    password: passwordSchema,
+
+    confirmPassword: z.string().min(1, 'Confirm password is required'),
+
+    country: z.string()
+      .optional()
+      .transform(val => val?.trim()),
+
+    phoneNumber: z.string()
+      .optional()
+      .transform(val => val?.trim())
+      .refine(val => val?.trim()),
+}).refine(data => data.password === data.confirmPassword, {
+  message: 'Passwords don\'nt match',
+  path: ['confirmPassword']
+});
+
+const updateProfileSchema = z.object({
+  firstName: z.string()
+    .min(1, 'First name is required')
+    .max(50)
+    .trim()
+    .optional(),
+    
+  lastName: z.string()
+    .min(1, 'Last name is required')
+    .max(50)
+    .trim()
+    .optional(),
+    
+}).partial();
+
+module.exports = {
+  registerSchema,
+  updateProfileSchema,
+  emailSchema,
+  usernameSchema,
+  passwordSchema,
+
+  validateRegister: (data) => registerSchema.parse(data),
+  validateUpdateProfile: (data) => updateProfileSchema.parse(data),
+};
