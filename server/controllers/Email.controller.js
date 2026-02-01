c427c5d23a8ac8cb24ed42c60bb09c9444848bba
Nikola Uzunov <nikollla2000@abv.bg>
2026-02-01 21:14:59 +0200

    Update Email.controller.js

--- server/controllers/Email.controller.js
+++ server/controllers/Email.controller.js
@@ -27,6 +27,9 @@ const sendEmail = async (req, res) => {
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
+            },
+            tls: {
+                rejectUnauthorized: false, //TODO
             }
         });
 
