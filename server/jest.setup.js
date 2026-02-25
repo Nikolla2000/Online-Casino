64878c837bde88ab5cd0b34bc0c0cc71ed1550f7
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2026-02-25 18:36:50 +0200

    fix jest setup - redis mockig

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,14 +1,29 @@
-// Mock Redis за тестове
-jest.mock('./config/redis', () => ({
+// // Mock Redis за тестове
+// jest.mock('./config/redis', () => ({
+//   get: jest.fn(),
+//   set: jest.fn(),
+//   setEx: jest.fn(),
+//   del: jest.fn(),
+//   incr: jest.fn().mockResolvedValue(1),
+//   decr: jest.fn().mockReturnValue(0),
+//   pExpire: jest.fn().mockResolvedValue('OK'),
+//   pTTL: jest.fn().mockResolvedValue(60000),
+// }));
+
+//purvata chast e gore predniqt variant
+
+const mockRedis = {
   get: jest.fn(),
   set: jest.fn(),
   setEx: jest.fn(),
   del: jest.fn(),
-  incr: jest.fn(),
-  decr: jest.fn(),
-  pExpire: jest.fn(),
-  pTTL: jest.fn(),
-}));
+  incr: jest.fn().mockResolvedValue(1), // Връща число
+  decr: jest.fn().mockResolvedValue(0), // Връща число
+  pExpire: jest.fn().mockResolvedValue('OK'),
+  pTTL: jest.fn().mockResolvedValue(60000), // Връща число (TTL в ms)
+};
+
+jest.mock('./config/redis', () => mockRedis)
 
 // Увеличи timeout за async тестове
 jest.setTimeout(10000);
