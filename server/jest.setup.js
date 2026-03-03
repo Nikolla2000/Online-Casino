9882602c038931917e1fdd7f97efc6ead7cba15c
Nikola Uzunov <nikollla2000@abv.bg>
2026-03-03 22:24:40 +0200

    jest setup

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,17 +1,3 @@
-// // Mock Redis за тестове
-// jest.mock('./config/redis', () => ({
-//   get: jest.fn(),
-//   set: jest.fn(),
-//   setEx: jest.fn(),
-//   del: jest.fn(),
-//   incr: jest.fn().mockResolvedValue(1),
-//   decr: jest.fn().mockReturnValue(0),
-//   pExpire: jest.fn().mockResolvedValue('OK'),
-//   pTTL: jest.fn().mockResolvedValue(60000),
-// }));
-
-//purvata chast e gore predniqt variant
-
 const mockRedis = {
   get: jest.fn(),
   set: jest.fn(),
@@ -23,6 +9,6 @@ const mockRedis = {
   pTTL: jest.fn().mockResolvedValue(60000),
 };
 
-jest.mock('./config/redis', () => mockRedis)
+jest.mock('./config/redis', () => mockRedis);
 
 jest.setTimeout(10000);
