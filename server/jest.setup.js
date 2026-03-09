b5825e73a9118f78fbef74bc328d3fcaf1250739
Nikola Uzunov <nikollla2000@abv.bg>
2026-03-09 22:57:00 +0200

    mongo db memory server setup in jest

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,3 +1,8 @@
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 const mockRedis = {
   get: jest.fn(),
   set: jest.fn(),
@@ -12,3 +17,25 @@ const mockRedis = {
 jest.mock('./config/redis', () => mockRedis);
 
 jest.setTimeout(10000);
+
+dotenv.config();
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+});
