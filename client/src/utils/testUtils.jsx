b6db2c4b878d8418c9ba5c8d2beb4c0ff107043e
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2026-03-24 16:55:33 +0200

    refactor renderWithProviders util test function and add documentation

--- client/src/utils/testUtils.jsx
+++ client/src/utils/testUtils.jsx
@@ -7,7 +7,15 @@ import aiChatbotReducer from '../redux/features/aiChatbot/aiChatbotSlice';
 import authModalsReducer from '../redux/features/auth/authModalsSlice'
 import { Toaster } from "react-hot-toast";
 
-//Redux wrapper function for testing
+
+/**
+ * Wraps React component with the necessary providers (Redux, React-Router) for testing.
+ * 
+ * @param {React.ReactElement} component - The component to be rendered for testing.
+ * @param {Object} [initialState={}] - Optional initial state to seed the Redux store.
+ * @returns {Object} An object containing the standard RTL render results and the created store instance.
+ * @property {Object} store - The Redux store instance used for the test (for store.getState() assertions).
+ */
 export const renderWithProviders = (component, initialState = {}) => {
     const store = configureStore({
         reducer: {
@@ -18,12 +26,15 @@ export const renderWithProviders = (component, initialState = {}) => {
         preloadedState: initialState
     });
     
-    return render(
-        <Provider store={store}>
-        <BrowserRouter>
-            {component}
-            <Toaster/>
-        </BrowserRouter>
-    </Provider>
-    );
+    return {
+        store,
+        ...render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    {component}
+                    <Toaster/>
+                </BrowserRouter>
+            </Provider>
+        )
+    }
 }
