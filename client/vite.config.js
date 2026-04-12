445c4b0fe4807a11ef3265f507fbb33c0fe7274f
Nikola Uzunov <nikollla2000@abv.bg>
2026-04-12 18:18:40 +0300

    fix sass deprecation issues and update vite
    - update nodejs too from 20 to 22
    - fix other modules issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/icons-material',
+      '@emotion/react',
+      '@emotion/styled',
+    ],
+  },
+  css: {
+    preprocessorOptions:{
+      scss:{
+        api: 'modern-compiler',
+        quietDeps: true,
+      }
+    }
+  }
 })
